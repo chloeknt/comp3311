320afa53c0cdbc707b75d1a2f7038e4db8f22c5f
chloeknt <chloe.k.toh@gmail.com>
2022-11-29 10:58:28 +1100

    Delete q7 directory

--- q7/q7.sql
+++
@@ -1,8 +0,0 @@
--- COMP3311 20T3 Final Exam Q7
--- Put any helper views or PLpgSQL functions here
--- You can leave it empty, but you must submit it
-
-create or replace view goalsOrderedByDate
-as
-select Goals.scoredin, Goals.scoredby, Matches.playedon, count(*) from Goals join Matches on Goals.scoredin = Matches.id group by Goals.scoredin, Goals.scoredby, Matches.playedon order by Matches.playedon
-;
--- q7/tests/01.sh
+++
@@ -1,1 +0,0 @@
-./q7 'Michael Baumann'
